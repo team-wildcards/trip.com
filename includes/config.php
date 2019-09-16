66bc5d69d7df2e3c70519370154508c16893c3ae
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-16 16:59:20 +0100

    Login & Sign up DB implementation

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Database Options
+$databaseHost = 'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+$databaseName = 'pien6ms8lawzyez3';
+$databaseUser = 'il7pn62806b6vf2v';
+$databasePassword = 'vpwygbn86aijnzy7';
+
+//Website
+$baseUrl = 'https://trips-com.herokuapp.com';
