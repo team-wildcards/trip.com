a0aa49422cd48307f78dcfc7464fb353b9e3d5fe
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2019-09-17 20:48:30 +0100

    Add shared components to success page

---
+++ success.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome Back to Trip</title>
+
+		<?php include('./shared/head.innerhtml.php'); ?>
+
+		<!-- scripts -->
+		<script src="https://kit.fontawesome.com/0807ea9fce.js"></script>
+
+		<!-- styles -->
+		<link rel="stylesheet" href="./assets/css/success.css">
+	</head>
+	<body>
+		<?php include('./shared/titlebar.php'); ?>
+
+		<!-- 
+		<header>
+			<img src="./assets/images/TripDotCom-01.svg" alt="Trip Logo">
+		</header>
+		 -->
+
+		<div class="card">
+			<!-- We'll use the .fa-icon class to add custom styling to font awesome icons. -->
+			<i class="fa-icon fas fa-check-circle"></i>
+			<h3 class="content"><strong>Your Sign in was successful!</strong></h3>  
+		</div>
+	</body>
+</html>
