7ca612e21bfbf15e547673c82731655a62ae8e9d
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-18 10:08:43 +0100

    fix logout

--- logout.php
+++ logout.php
@@ -10,7 +10,7 @@ session_start();
 
         session_destroy();
 
-        echo '<meta http-equiv="refresh" content="2;url=index.html">';
+        echo '<meta http-equiv="refresh" content="2;url=index.php">';
         echo '<progress max=100><strong>Progress: 60% done. </strong></progress><br>';
         echo '<span class="itext">Logging out please wait!...</span>';
 
